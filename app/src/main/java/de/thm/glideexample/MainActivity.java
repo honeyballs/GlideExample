3ee96a1952a9936161cd6df873f4fda1cbbbe128
Yannick Bals <yannick.bals@web.de>
2018-06-04 11:01:55 +0200

    Init project

---
+++ app/src/main/java/de/thm/glideexample/MainActivity.java
@@ -0,0 +1,12 @@
+package de.thm.glideexample;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yannick Bals on 04.06.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
